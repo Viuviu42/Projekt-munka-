602f22cbf6a695f707295337ba463e29a8893345
Viuviu42 <116081327+Viuviu42@users.noreply.github.com>
2024-03-17 18:31:43 +0100

    Class added + 1,2 task completed

--- telefon.py
+++ telefon.py
@@ -1,9 +1,24 @@
+class Timedates:
+    def __init__(self, h, min, sec):
+        self.sec = sec
+        self.min = min
+        self.h = h
+
+    def __str__(self):
+        return f"{self.sec} {self.min} {self.h}"
+
+    def masodperc(self):
+        return self.h*60*60+self.min*60+self.sec
+
+
 file = open("hivas.txt", "rt", encoding="utf-8")
-timedates = []
+starttimedates = []
+endtimedates = []
 
 for row in file:
     i = row.strip().split(" ")
-    timedates.append(i)
+    starttimedates.append(Timedates(i[0], i[1], i[2]))
+    endtimedates.append(Timedates(i[3], i[4], i[5]))
 file.close()
 
 
